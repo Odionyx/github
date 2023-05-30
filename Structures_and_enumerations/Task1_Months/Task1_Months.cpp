82de4b5bcf891fe5141972f89974a36f8bc10eb8
Maks_Nik_CPP7 <odionyx@yandex.ru>
2023-05-30 21:48:11 +0400

    Add Task1_Months

--- Structures_and_enumerations/Task1_Months/Task1_Months.cpp
+++ Structures_and_enumerations/Task1_Months/Task1_Months.cpp
@@ -1,20 +1,76 @@
-﻿// Task1_Months.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿// Task1_Months.cpp
 #include <iostream>
 
-int main()
+enum class months
 {
-    std::cout << "Hello World!\n";
+	unknown,
+	january,
+	february,
+	march,
+	april,
+	may,
+	june,
+	july,
+	august,
+	september,
+	october,
+	november,
+	december,
+};
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "ru_RU");
+	int m = -1;
+
+	while (m != static_cast <int> (months::unknown)) {
+		std::cout << "Введите номер месяца: ";
+		std::cin >> m;
+		switch (m)
+		{
+		case (static_cast <int> (months::unknown)):
+			std::cout << "До свидания " << std::endl;
+			break;
+		case (static_cast <int> (months::january)):
+			std::cout << "Январь" << std::endl;
+			break;
+		case (static_cast <int> (months::february)):
+			std::cout << "Февраль" << std::endl;
+			break;
+		case (static_cast <int> (months::march)):
+			std::cout << "Март" << std::endl;
+			break;
+		case (static_cast <int> (months::april)):
+			std::cout << "Апрель" << std::endl;
+			break;
+		case (static_cast <int> (months::may)):
+			std::cout << "Май" << std::endl;
+			break;
+		case (static_cast <int> (months::june)):
+			std::cout << "Июнь" << std::endl;
+			break;
+		case (static_cast <int> (months::july)):
+			std::cout << "Июль" << std::endl;
+			break;
+		case (static_cast <int> (months::august)):
+			std::cout << "Август" << std::endl;
+			break;
+		case (static_cast <int> (months::september)):
+			std::cout << "Сентябрь" << std::endl;
+			break;
+		case (static_cast <int> (months::october)):
+			std::cout << "Октябрь" << std::endl;
+			break;
+		case (static_cast <int> (months::november)):
+			std::cout << "Ноябрь" << std::endl;
+			break;
+		case (static_cast <int> (months::december)):
+			std::cout << "Декабрь" << std::endl;
+			break;
+		default:
+			std::cout << "Неправильный номер!" << std::endl;
+			break;
+		}
+	}
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
