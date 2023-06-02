a7895fb0ac1b030be4a5ec09d2c0bd4cba097d82
Maks_Nik_CPP7 <odionyx@yandex.ru>
2023-06-02 05:40:46 +0400

    Add Task2 Task3

---
+++ Structures_and_enumerations/Task2_Accounts/Task2_Accounts.cpp
@@ -0,0 +1,50 @@
+﻿// Task2_Accounts.cpp
+#include <iostream>
+#include <windows.h> /*system*/// для SetConsoleCP(1251);
+
+struct person        //Создаем структуру!
+{
+    int num;               // номер счёта (целое число)
+    std::string owner;     // здесь будет храниться имя владельца
+    double value;          // количество денег на счету (дробное число)
+};
+
+// Функция должна принимать экземпляр структуры банковского счёта и новую сумму.
+double data_input(double* const value,const double& i) 
+{
+    return *value = i;
+}
+
+int main()
+{
+    setlocale(LC_ALL, ".1251");
+    SetConsoleCP(1251);                   //задаем кодировку для вывода символов на экран
+
+    person pers = { 0, "Name", 0.0};      //Инициализация структуры
+    double* ptr_value = &pers.value;
+    std::string str[5] = 
+    {"номер счёта: ", "имя владельца: ", "баланс: ", "новый баланс: ", "Ваш счёт: "};
+
+    for (int x = 0; x < 4; x++)
+    {
+        std::cout << "Введите ";
+        if (!x) {
+            std::cout << str[x];
+            std::cin >> pers.num;
+        }
+        else if (x == 1) {
+            std::cout << str[x];
+            std::cin >> pers.owner;
+        }
+        else {
+            double i = 0.0;
+            std::cout << str[x];
+            std::cin >> i;
+            data_input(ptr_value, i);              // функция
+        }
+        // std::cout << std::endl;
+    }
+    std::cout << str[4] << pers.owner << ", " << pers.num << ", " << pers.value << std::endl;
+    return 0;
+}
+// Thanks for the tutorial!
---
+++ Structures_and_enumerations/Task3_Structure_output/Task3_Structure_output.cpp
@@ -0,0 +1,51 @@
+﻿// Task3_Structure_output.cpp
+#include <iostream>
+
+struct address       //Создаем структуру!
+{
+    std::string city;
+    std::string street;
+    int house_num;
+    int apartment;
+    int index;
+};
+                     // Функция 
+void print_screen(std::string* const c, std::string* const s, int* const h, int* const a, int* const i)
+{
+    std::string str[5] =
+    { "Город: ", "Улица: ", "Номер дома: ", "Номер квартиры: ", "Индекс: "};
+    std::cout << str[0] << "\t" << *c << std::endl;
+    std::cout << str[1] << "\t" << *s << std::endl;
+    std::cout << str[2] << *h << std::endl;
+    std::cout << str[3] << *a << std::endl;
+    std::cout << str[4] << *i << std::endl;
+    std::cout << std::endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, ".1251");
+    address addr = { "City", "Street", 0, 0, 0};      //Инициализация структуры
+
+    std::string* p_city = &addr.city;
+    std::string* p_str = &addr.street;
+    int* p_house_num = &addr.house_num;
+    int* p_apartment = &addr.apartment;
+    int* p_index = &addr.index;
+
+    addr.city = "Москва";
+    addr.street = "Арбат";
+    addr.house_num = 12;
+    addr.apartment = 8;
+    addr.index = 123456;
+    print_screen(p_city, p_str, p_house_num, p_apartment, p_index);
+
+    addr.city = "Ижевск";
+    addr.street = "Пушкина";
+    addr.house_num = 59;
+    addr.apartment = 143;
+    addr.index = 953769;
+    print_screen(p_city, p_str, p_house_num, p_apartment, p_index);
+    return 0;
+}
+// Thanks for the tutorial!
