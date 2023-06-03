7754dcecad230d9e910cb7fd0983dc61ca479fcc
Maks_Nik_CPP7 <odionyx@yandex.ru>
2023-06-03 21:59:01 +0400

    Add classes

---
+++ Classes/Task1_сalculator_of_two_numbers/Task1_сalculator_of_two_numbers.cpp
@@ -0,0 +1,94 @@
+﻿// Task1_сalculator_of_two_numbers.cpp :
+#include <iostream>
+
+class Calculator
+{
+private:
+    double num1;
+    double num2;
+public:
+    Calculator(double num1, double num2) {
+        this-> num1 = num1;
+        this-> num2 = num2;
+    }
+    //* метод сложения num1 и num2
+    double add(const double* num1, const double* num2) {
+        return (*num1) + (*num2);
+    }
+    //* метод перемножения num1 и num2
+    double multiply(const double* num1, const double* num2) {
+        return (*num1) * (*num2);
+    }
+    //* метод вычитания num2 из num1
+    double subtract_1_2(const double* num1, const double* num2) {
+        return (*num1) - (*num2);
+    }
+    //* метод вычитания num1 из num2
+    double subtract_2_1(const double* num1, const double* num2) {
+        return (*num2) - (*num1);
+    }
+    //* метод деления num1 на num2
+    double divide_1_2(const double* num1, const double* num2) {
+        return (*num1)/(*num2);
+    }
+    //* метод деления num2 на num1
+    double divide_2_1(const double* num1, const double* num2) {
+        return (*num2)/(*num1);
+    }
+    /*  метод значение поля num1 равное num1, если значение не равно 0.
+        Возвращает true, если успешно, в обратном случае false */
+    bool set_num1(const double* num1) {
+        if (static_cast <bool>(*num1)) {
+            this -> num1 = *num1;
+            return true;
+        } else { return false;}
+    }
+    /* метод значение поля num2 равное num2, если значение не равно 0.
+        Возвращает true, если успешно, в обратном случае false */
+    bool set_num2(const double* num2) {
+        if (static_cast <bool>(*num2)) {
+            this -> num2 = *num2;
+            return true;
+        } else { return false;}
+    }
+};
+
+void print_screen(bool b, char ch, double d)
+{
+    std::cout<<"num"<<(b ? "1" : "2")<<" "<< ch <<" num"<<(b ? "2" : "1")<<" = "<< d << std::endl;
+}
+
+void calc( void)
+{
+    double num1 = 0.0;
+    double num2 = 0.0;
+    Calculator calc = Calculator(num1, num2);
+    while (1) {
+        std::cout << "Введите num1: ";
+        std::cin >> num1;
+        if (calc.set_num1(&num1)) {
+            while (1) {
+                std::cout << "Введите num2: ";
+                std::cin >> num2;
+                if (calc.set_num2(&num2)) {
+                    print_screen(true,  '+', calc.add           (&num1, &num2));
+                    print_screen(true,  '-', calc.subtract_1_2  (&num1, &num2));
+                    print_screen(false, '-', calc.subtract_2_1  (&num1, &num2));
+                    print_screen(true,  '*', calc.multiply      (&num1, &num2));
+                    print_screen(true,  '/', calc.divide_1_2    (&num1, &num2));
+                    print_screen(false, '/', calc.divide_2_1    (&num1, &num2));
+                }
+                else { std::cout << "Неверный ввод!" << std::endl; continue;}
+                break;
+            }
+        } else { std::cout << "Неверный ввод!" << std::endl; continue;}
+    }
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, ".1251");
+    calc();
+    return 0;
+}
+// Thanks for the tutorial!
---
+++ Classes/Task2_сounter/Task2_сounter.cpp
@@ -0,0 +1,90 @@
+﻿// Task2_сounter.cpp :
+#include <iostream>
+#include <Windows.h>
+
+class Counter
+{
+private:
+    int num;
+    int num2;
+public:
+    //* метод ++
+    int inc(void) {
+        return num++;
+    }
+    //* метод --
+    int dec(void) {
+        return num--;
+    }
+    //* метод initial point
+    int set_count(const int* const num2) {
+        return num = *num2;
+    }
+    //* метод = equal
+    void get_count(void) {
+        std::cout << num << std::endl;
+        return;
+    }
+    Counter(void) {
+        this->num = 1;
+    }
+    Counter(const int* const num2) {
+        this->num = *num2;
+    }
+    /*
+    Обратите внимание на то, что у внешнего кода не должно быть 
+    возможности изменить внутреннее значение счётчика напрямую ??
+    */
+};
+
+void counter()
+{
+    int num = 0;
+    int num2 = 0;
+    char ch = ' ';
+
+    std::string str;
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> str;
+    int size = str.size();
+
+    Counter count;
+    if (size == 2) {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> num;
+        num = count.set_count(&num);
+        Counter::Counter(&num);
+    }
+    if (size == 3) {
+        Counter count = Counter::Counter();
+    }
+    while (ch != 'х') {
+        std::cout << "Введите команду('+', '-', '=' или 'x'): ";
+        std::cin >> ch;
+        switch (ch)
+        {
+        case '+': //* увеличьте счётчик на 1
+            count.inc();
+            break;
+        case '-': //* уменьшите счётчик на 1
+            count.dec();
+            break;
+        case '=': //* выведите текущее значение счётчика
+            count.get_count();
+            break;
+        case 'х': //* завершите работу программы
+            std::cout << "До свидания!" << std::endl;
+            break;
+        }
+    }
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, ".1251");
+    SetConsoleCP(1251);
+
+    counter();
+    return 0;
+}
+// Thanks for the tutorial!
