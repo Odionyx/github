4135cc116f42716b75fb4bae2de7b25261928c85
Maks_Nik_CPP7 <odionyx@yandex.ru>
2023-06-09 17:09:20 +0400

    Change Task

--- OOP_Abstract_encapsul/Task1_Addresses/Task1_Addresses.cpp
+++ OOP_Abstract_encapsul/Task1_Addresses/Task1_Addresses.cpp
@@ -24,7 +24,7 @@ public:
         this->apartment = *apartment;
         return;
     }
-    void get_address(std::ofstream& fout) {
+    void print_address(std::ofstream& fout) {
         fout << this->city << ", " << this->street << ", " << this->house << ", " << this->apartment;
         return;
     }
@@ -46,17 +46,22 @@ int main(int argc, char** argv)
     std::ifstream fin("in.txt");
     if (!(fin.is_open())) {
         std::cout << "Error reading from file." << std::endl;
+        adr = nullptr;
         return 1;
     }
     else {
         if (!(fin >> size)) {
             std::cout << "Error reading size from file." << std::endl;
+            adr = nullptr;
+            return 1;
         }
         //* проверка размера
         if (0 >= size) {
             std::cout << "Negative or zero size." << std::endl;
+            adr = nullptr;
+            return 1;
         }
-        adr = new Address[3]{};
+        adr = new Address[size];
         std::string str = "abc";
         for (int x = 0; (x < size); x++) {
             fin >> c >> s >> h >> a;
@@ -75,7 +80,7 @@ int main(int argc, char** argv)
     else {
         fout << size << std::endl;
         for (int x = (size-1); 0 <= x ; x--) {
-            adr[x].get_address(fout);
+            adr[x].print_address(fout);
             fout << std::endl;
         }
         fout.close();
